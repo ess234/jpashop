08227795945ed179c8bd1974711dac7e41a2de48
양진윤 <yangjin-yun@yangjin-yun-ui-MacBookPro.local>
2020-01-06 23:34:43 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+//DTO 역할
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findId(Long id){
+        Member findMember = em.find(Member.class, id);
+        return findMember;
+    }
+}
