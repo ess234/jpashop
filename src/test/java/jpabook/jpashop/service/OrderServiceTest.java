3fe507de11d46220f1d74f0aa89391455685d261
yangjin-yun <ess234@naver.com>
2020-02-16 23:43:19 +0900

    1. 상품 레파지토리 생성 2. 상품 서비스 생성 3. 주문 레파이토리 생성 4. 주문 서비스 생성 5. 상품&주문 테스트 코드 작성 6. 주문 아이템, 주문 생성메소드 생성

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceTest {
+
+    @Test
+    public void order() {
+    }
+
+    @Test
+    public void cancle() {
+    }
+}
